f1715976d7ab57e5328642f797644e07fc38b5dc
Kristin Henno <kristinhenno@gmail.com>
2017-09-18 09:11:29 -0700

    k

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-
     console.log("hi");
 
     $("#signupbutton").on("click", function(event) {
-                event.preventDefault();
+        event.preventDefault();
         console.log("hi2");
 
+        var count = 0;
 
         var username = $("#usernameInput").val();
         var name = $("#nameInput").val();
@@ -15,52 +15,66 @@ $(document).ready(function() {
         var bio = $("#bioInput").val();
         var gender = $("#custom-select").val();
 
-        localStorage.setItem('user', username);
-
-        if (isNaN(age) == false) {
-
-
-            var newUser = {
-                username: username,
-                password: password,
-                name: name,
-                age: age,
-                bio: bio,
-                gender: gender
+        $.get("/api/usernames/", function(data) {
+            for (i = 0; i < data.length; i++) {
+                if (username === data[i].username) {
+                    count++;
+                }
             }
+            if (count === 0) {
+                localStorage.setItem('user', username);
 
-            function sayHi() {
-                console.log("hi");
-            };
+                if (isNaN(age) == false) {
 
 
-            var password = $("#passwordInput").val();
-            var password1 = $("#password1").val();
+                    var newUser = {
+                        username: username,
+                        password: password,
+                        name: name,
+                        age: age,
+                        bio: bio,
+                        gender: gender
+                    }
 
-            // console.log(password);
+                    function sayHi() {
+                        console.log("hi");
+                    };
 
-            if (password === password1) {
-                console.log("passwords match");
-                $.post("/api/new", newUser)
-                    // On success, run the following code
-                    .then(function(data) {
-                        // Log the data we found
-                        console.log(data);
-                        window.location.href = '/picture';
-                    }).fail(function(Error){
-                        console.log(Error);
-                    });
+
+                    var password = $("#passwordInput").val();
+                    var password1 = $("#password1").val();
+
+                    // console.log(password);
+
+                    if (password === password1) {
+                        console.log("passwords match");
+                        $.post("/api/new", newUser)
+                            // On success, run the following code
+                            .then(function(data) {
+                                // Log the data we found
+                                console.log(data);
+                                window.location.href = '/picture';
+                            }).fail(function(Error) {
+                                console.log(Error);
+                            });
+
+                    } else {
+                        alert("passwords dont match");
+                        console.log("dont match");
+                    }
+
+                } else {
+                    alert("Age must be a number");
+                }
 
             } else {
-                alert("passwords dont match");
-                console.log("dont match");
+                alert("Username taken. Try a new one")
             }
 
-        } else {
-            alert("Age must be a number");
-        }
 
-    })
+        });
+
+    });
 
 
 });
